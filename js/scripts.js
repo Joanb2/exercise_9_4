6109c5cd39ba50dac62f29be70c50d08105b0de0
Joanna Buchała <joanna.b2@op.pl>
2018-04-06 21:04:29 +0200

    add var's

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
-var i = "rows";
-var k = "spaces";
-var j = "stars";
-
 function drawTree(height) {
-	for (i = 1 ; i <= height ; i++) {
+	for (var i = 1 ; i <= height ; i++) {
 		var star ="";
 
-		for (k = i ; k <= height ; k++){
+		for (var k = i ; k <= height ; k++){
 			star +=" ";
 		}
-		for (j = 0 ; j < (i*2) -1 ; j++){
+		for (var j = 0 ; j < (i*2) -1 ; j++){
 			star += "*";
 		}
 		console.log(star);
